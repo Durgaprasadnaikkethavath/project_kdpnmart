f5dd46525d779d17da9d436cafa28ac67db45d22
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-20 16:11:08 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
+import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./Context/ShopContext.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter basename="/project_kdpnmart/">
       <ShopContextProvider>
         <App />
       </ShopContextProvider>
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );

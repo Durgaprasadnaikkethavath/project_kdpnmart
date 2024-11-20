a2147822f57de885b9779c8a81e9456047dea572
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-20 16:06:10 +0530

    next

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,10 @@ import ShopContextProvider from "./Context/ShopContext";
 const App = () => {
   return (
     <>
-      <ShopContextProvider>
-        <h1>Carousel images</h1>
-        <h1>Next Carousel</h1>
-        <Navbar />
-        <ImageCarousel />
-      </ShopContextProvider>
+      <h1>Carousel images</h1>
+      <h1>Next Carousel</h1>
+      <Navbar />
+      <ImageCarousel />
     </>
   );
 };

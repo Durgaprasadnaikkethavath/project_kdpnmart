9b5415967a17adfffa6f09a67b6e5a32db475f4a
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-20 15:53:25 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import ImageCarousel from "./Components/ImageCarousel";
+import ShopContextProvider from "./Context/ShopContext";
 
 const App = () => {
   return (
     <>
-      <h1>Carousel images</h1>
-      <Navbar />
-      <ImageCarousel />
+      <ShopContextProvider>
+        <h1>Carousel images</h1>
+        <Navbar />
+        <ImageCarousel />
+      </ShopContextProvider>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, HashRouter } from "react-router-dom";
-import ShopContextProvider from "./Context/ShopContext.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <ShopContextProvider>
+  <StrictMode>
+    <BrowserRouter>
       <App />
-    </ShopContextProvider>
-  </HashRouter>
+    </BrowserRouter>
+  </StrictMode>
 );

ced6ff087a57126e4476b5224590a9c274a6acf7
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-20 15:08:28 +0530

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Offer from "./Offer";
+
+const Navbar = () => {
+  return (
+    <>
+      <Offer />
+      <h1>Navbar</h1>
+    </>
+  );
+};
+
+export default Navbar;
